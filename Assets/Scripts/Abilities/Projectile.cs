8b9451b2f13ce51489e180754fcf3350b1b1cce4
Kostya Yeremenko <KostYer@yahoo.com>
2025-11-23 18:56:06 +0200

    [ADD] fire ability basic

---
+++ Assets/Scripts/Abilities/Projectile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Abilities
+{
+    public class Projectile: MonoBehaviour
+    {
+        private float _explodeForce;
+        private float _explosionRadius = 10f;
+        [SerializeField] private Rigidbody _rb;
+        [SerializeField] private LayerMask _affectsLayers;
+
+        private void OnValidate()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        public void OnShoot(Vector3 moveForce, float explodeForce)
+        {
+            _explodeForce = explodeForce;
+            _rb.AddForce(moveForce, ForceMode.Impulse);
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Explode();
+        }
+
+        private void Explode()
+        {
+            Collider[] hits = Physics.OverlapSphere(transform.position, _explosionRadius, _affectsLayers);
+
+            foreach (var hit in hits)
+            {
+                Rigidbody rb = hit.attachedRigidbody;
+                if (rb == null) continue;
+
+                // Apply explosion force
+                rb.AddExplosionForce(
+                    _explodeForce,
+                    transform.position,
+                    _explosionRadius,
+                    0.5f, // optional upward modifier
+                    ForceMode.Impulse
+                );
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/ProjectileAbility.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Abilities
+{
+    public class ProjectileAbility: MonoBehaviour
+    {
+        [SerializeField]  private float _speed = 550f;
+        [SerializeField]   private float _explodeForce = 40f;
+
+        [SerializeField] private Transform _shootPoint;
+        [SerializeField] private Projectile _projectile;
+        
+        private void Shoot()
+        {
+            var go = Instantiate(_projectile, _shootPoint.position, _shootPoint.rotation);
+            var force = transform.forward * _speed;
+            go.OnShoot(force, _explodeForce);
+        }
+
+
+        public void OnFireAnimCallback()
+        {
+            Shoot();
+        }
+
+
+
+    }
+}
